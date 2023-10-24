e8289d6da448b0ef1213cf2cb429010d70422a1a
Rajat <rwlrajat@gmail.com>
2023-10-24 16:54:35 +0530

    updated app

--- app.py
+++ app.py
@@ -61,17 +61,18 @@ col1,col2 = st.columns(2)
 with col1:
     batting_team = st.selectbox('Select Batting Team ' , teams)
 with col2:
+  
     bowling_team = st.selectbox('Select Bowling Team ',teams)
     
 city = st.selectbox('Select City',cities)
 col3,col4,col5 = st.columns(3)
 
 with col3:
-    current_score =st.number_input('Current Score')
+    current_score =st.number_input('Current Score',min_value=0)
 with col4:
-    overs_done = st.number_input('Overs Done (Must Be Greater Than 5 )')
+    overs_done = st.number_input('Overs Done (Must Be Greater Than 5 )',min_value=5,max_value=20,step=1)
 with col5:
-    wickets = st.number_input('Wickets Taken')
+    wickets = st.number_input('Wickets Taken',max_value=9)
     
 runs_in_5 = st.number_input('Runs In Last 5 Overs')
 
